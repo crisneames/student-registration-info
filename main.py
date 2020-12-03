44d1e45f099708acd4d8b1536dca57a2307c312e
Suresh Melvin Sigera <sureshsigera@gmail.com>
2020-12-03 16:01:02 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,124 @@
+data = {
+	10001:
+		{
+			'first_name': 'Chris',
+			'last_name': 'Herzog',
+			'email': 'chriher22@aol.com',
+			'classes_taken': ('CSC126', 'CSC211', 'CSC326'),
+			'grades_received': ('A', 'C', 'F'),
+			'type': 'student'
+		},
+	10002:
+		{
+			'first_name': 'Joseph',
+			'last_name': 'Smith',
+			'email': 'JosephSSmith@rhyta.com',
+			'classes_taken': ('CSC456', 'CSC121', 'ENG151'),
+			'grades_received': ('A', 'C', 'A'),
+			'type': 'student'
+		},
+	10003:
+		{
+			'first_name': 'Kelli',
+			'last_name': 'Marshall',
+			'email': 'JosephSSmith@rhyta.com',
+			'classes_taken': ('CSC456', 'CSC121', 'ENG120', 'ENG121', 'ENG151', 'ENG230'),
+			'grades_received': ('A', 'A', 'A', 'A', 'A', 'A'),
+			'type': 'student'
+		},
+	10004:
+		{
+			'first_name': 'Manuel',
+			'last_name': 'Horn',
+			'email': 'ManuelCHorn@dayrep.com',
+			'classes_taken': ('CSC456', 'CSC121'),
+			'grades_received': ('A', 'C'),
+			'type': 'student'
+		},
+	10005:
+		{
+			'first_name': 'Kevin',
+			'last_name': 'Lacour',
+			'email': 'KevinBLacour@teleworm.us',
+			'classes_taken': ('CSC211', 'CSC220'),
+			'grades_received': ('F', 'F'),
+			'type': 'student'
+		},
+	10006:
+		{
+			'first_name': 'Suresh',
+			'last_name': 'Sigera',
+			'email': 'sureshsigera@ms.edu',
+			'password': 'J&hqweh12e7d2n281',
+			'type': 'staff'
+		},
+	10007:
+		{
+			'first_name': 'Ann',
+			'last_name': 'Chatman',
+			'email': 'AnnBChatman@ms.edu',
+			'password': 'J&hqweh12e7d2n28qq1',
+			'type': 'staff'
+		},
+}
+
+
+def admin_login():
+	"""
+	validate administrator (staff) login credentials
+	if the login is successful, show the menu to access the following functionality:
+	add_new_student, list_all_students, search_students, remove_student and display_single_record
+	"""
+
+
+def add_new_student():
+	"""
+	add new student record to the data dictionary
+	"""
+	pass
+
+
+def list_all_students():
+	"""
+	list all the student records from the data dictionary
+	for each student, show the following:
+	first_name, last_name, email, classes_taken, grades_received and the gpa
+	"""
+	pass
+
+
+def search_students_():
+	"""
+	search student records by student id or by email address and print the first and last name
+	"""
+	pass
+
+
+def remove_student():
+	"""
+	remove a student record from the data dictionary
+	"""
+	pass
+
+
+def display_single_record():
+	"""
+	display a single student record by ID
+	show the following:
+	first name, last name, email, classes_taken, grades_received and the gpa
+	"""
+	pass
+
+
+def main():
+	"""
+	function calls
+	"""
+	pass
+
+
+# the driver code
+if __name__ == '__main__':
+	main()
+  
+  
